4ef51b4bda65f400e68c1b97e313d602769f8aae
weixinwei123 <2471583935@qq.com>
2022-03-04 15:31:28 +0800

    主干添加

--- ddd/src/main/java/com/hellogit.java
+++ ddd/src/main/java/com/hellogit.java
@@ -5,6 +5,7 @@ public class hellogit {
     public static void main(String[] args){
         System.out.println("heelo git");
         System.out.println("创建分支");
+        System.out.println("主干添加");
 
     }
 }

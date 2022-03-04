96520e40b8576096c42f6ca264efcc5af9058c38
weixinwei123 <100904664+weixinwei123@users.noreply.github.com>
2022-03-04 16:13:53 +0800

    Update hellogit.java

--- ddd/src/main/java/com/hellogit.java
+++ ddd/src/main/java/com/hellogit.java
@@ -7,6 +7,7 @@ public class hellogit {
         System.out.println("创建分支");
         System.out.println("主干添加");
         System.out.println("分支添加");
+        System.out.println("更新下载");
 
     }
 }
